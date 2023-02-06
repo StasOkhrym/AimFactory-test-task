f9b63a956954e15a4d77d24a645725a6aac475d5
Stas Okhrym <s.okhrym@gmail.com>
2023-02-06 10:14:24 +0200

    Updated url.py

--- api/urls.py
+++ api/urls.py
@@ -5,7 +5,7 @@ from api.views import genre_list_view, movie_list_view, movie_detail_view
 urlpatterns = [
     path("genres/", genre_list_view, name="genres_list"),
     path("movies/", movie_list_view, name="movies_list"),
-    path("movies/<int:pk>", movie_detail_view, name="movie_detail"),
+    path("movies/<int:pk>/", movie_detail_view, name="movie_detail"),
 ]
 
 app_name = "api"
